349a4acc1cfc54542e440936d82c1245c4e4359f
Timothy Totten <2009@huri.net>
2012-02-14 16:16:10 -0800

    Added a no-cache meta extension which disables HTTP caching.

---
+++ lib/nano2/meta/no-cache.php
@@ -0,0 +1,10 @@
+<?php
+
+/* If you load this Meta library, we will set rules to
+   say this page is not to be cached. This is useful for
+   dynamic content such as data being sent via AJAX calls, etc.
+ */
+
+header('Cache-Control: no-cache, must-revalidate'); // the standard.
+header('Expires: Thu, 22 Jun 2000 18:45:00 GMT');   // fixes IE bugs.
+
