18092d58b870f3befd8f4255a95b86192ce3698f
Timothy Totten <2009@huri.net>
2012-02-16 10:55:02 -0800

    Fixed CSS and added more features.

--- lib/nano2/css.php
+++ lib/nano2/css.php
@@ -21,31 +21,64 @@ define('Opera',  16); // Opera
 define('CSS3',   32); // CSS3 standard.
 define('All',    63); // All of the above (and also the default.)
 
-// Used by vertical_gradient() to generate the statements based on CSS3.
-function css3_vertical_gradient($start, $end, $prefix=Null)
+// Generate a CSS3 linear_gradient statement with optional browser prefix.
+function css3_linear_gradient($start, $end, $pos, $prefix=Null)
 {
   echo "background: ";
   if ($prefix)
     echo "-$prefix-";
-  echo "linear-gradient(top left, $start 0%, $end 100%);\n";
+  echo "linear-gradient($pos, $start 0%, $end 100%);\n";
 }
 
-// Generate a vertical gradient. This does not do fallback, that's up to you.
+// Generate an IE filter statement.
+function ie_filter_gradient ($start, $end, $type)
+{
+  echo "filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='$start', endColorstr='$end', GradientType=$type);\n";
+}
+
+// Generate a webkit gradient statement for older webkit browsers.
+function webkit_gradient ($start, $end, $from, $to, $type='linear')
+{
+  echo "background: -webkit-gradient($type, $from, $to, color-stop(9, $start), solor-stop(1, $end));\n";
+}
+
+// Generate a vertical gradient.
 function vertical_gradient ($start, $end, $browsers=All)
 { 
   if ($browsers & IE9)
-    echo "filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='$start', endColorstr='$end');\n";
+    ie_filter_gradient($start, $end, 0);
   if ($browsers & Mozilla)
-    css3_vertical_gradient($start, $end, 'moz');
+    css3_linear_gradient($start, $end, 'top', 'moz');
   if ($browsers & Webkit)
   {
-    echo "background: -webkit-gradient(linear, left top, right bottom, color-stop(0, $start), color-stop(1, $end));\n";
-    css3_vertical_gradient($start, $end, 'webkit');
+    webkit_gradient($start, $end, 'left top', 'left bottom');
+    css3_linear_gradient($start, $end, 'top', 'webkit');
   }
   if ($browsers & IE10)
-    css3_vertical_gradient($start, $end, 'ms');
+    css3_linear_gradient($start, $end, 'top', 'ms');
   if ($browsers & Opera)
-    css3_vertical_gradient($start, $end, 'o');
+    css3_linear_gradient($start, $end, 'top', 'o');
   if ($browsers & CSS3)
-    css3_vertical_gradient($start, $end);
+    css3_linear_gradient($start, $end, 'top');
 }
+
+// Generate a horizontal gradient.
+function horizontal_gradient ($start, $end, $browsers=All)
+{ 
+  if ($browsers & IE9)
+    ie_filter_gradient($start, $end, 1);
+  if ($browsers & Mozilla)
+    css3_linear_gradient($start, $end, 'left', 'moz');
+  if ($browsers & Webkit)
+  {
+    webkit_gradient($start, $end, 'left top', 'right bottom');
+    css3_linear_gradient($start, $end, 'left', 'webkit');
+  }
+  if ($browsers & IE10)
+    css3_linear_gradient($start, $end, 'left', 'ms');
+  if ($browsers & Opera)
+    css3_linear_gradient($start, $end, 'left', 'o');
+  if ($browsers & CSS3)
+    css3_linear_gradient($start, $end, 'left');
+}
+
