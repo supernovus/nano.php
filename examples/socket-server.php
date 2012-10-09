c6f6d7af55e3b8fedd17c676600d99e5d900c67e
Timothy Totten <2010@huri.net>
2012-10-09 12:08:41 -0700

    Added example socket server.

---
+++ examples/socket-server.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once 'lib/nano3/init.php';
+
+$s = new \Nano3\Utils\SocketDaemon(array('port'=>2525));
+$_server_listening = true;
+
+while ($_server_listening)
+{ $c = $s->accept();
+  if ($c === false)
+    usleep(100);
+  elseif ($c->socket > 0)
+  { $_client_listening = true;
+    do
+    { $command = trim($c->read(1024));
+      switch ($command) {
+        case 'QUIT':
+          $c->write("Shutting down\n");
+          $c->close();
+          $s->close(true,true);
+          $_client_listening = false;
+          $_server_listening = false;
+          break;
+        case 'END':
+          $c->write("Goodbye\n");
+          $c->close();
+          $_client_listening = false;
+          break;
+        default:
+          $c->write("ECHO $command\n");
+      }
+    } while ($_client_listening);
+    echo "Client exited\n";
+  }
+  else
+  { echo "error: ".$c->error_msg()."\n";
+    die;
+  }
+}
+echo "Server shutdown\n";
+
