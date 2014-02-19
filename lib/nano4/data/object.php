9d5e6271159782015375cd59fd3f5020f9c384f6
Timothy Totten <2010@huri.net>
2014-02-19 14:03:04 -0800

    Classinfo moved out of Data\Object into a Trait.

--- lib/nano4/data/object.php
+++ lib/nano4/data/object.php
@@ -43,6 +43,8 @@ namespace Nano4\Data;
 
 abstract class Object
 {
+  use \Nano4\Meta\ClassInfo;
+
   protected $data = array();   // The actual data we represent.
   protected $parent;           // Will be set if we have a parent object.
 
@@ -400,65 +402,6 @@ abstract class Object
     else
     {
       return $xmlstring;
-    }
-  }
-
-  // Return the lowercase "basename" of our class.
-  public function get_classname ($object=Null)
-  {
-    if (is_null($object))
-      $object = $this;
-    $classpath = explode('\\', get_class($object));
-    $classname = strtolower(end($classpath));
-    return $classname;
-  }
-
-  // Return the lowercase "dirname" of our class.
-  public function get_namespace ($object=Null)
-  {
-    if (is_null($object))
-      $object = $this;
-    $classpath = explode('\\', get_class($object));
-    array_pop($classpath); // Eliminate the "basename".
-    $namespace = join('\\', $classpath);
-    return $namespace;
-  }
-
-  /**
-   * Get either our parent class, or a parent class in our heirarchy with
-   * a given classname (as returned by get_classname(), so no Namespaces.)
-   */
-  public function get_parent ($class=Null)
-  {
-    $parent = $this->parent;
-    if (is_null($class))
-    {
-      return $parent;
-    }
-    else
-    { // The get_classname() function uses lowercase names.
-      $class = strtolower($class);
-    }
-
-    if (isset($parent))
-    {
-      if (is_callable(array($parent, 'get_classname')))
-      {
-        $parentclass = $parent->get_classname();
-      }
-      else
-      {
-        $parentclass = $this->get_classname($parent);
-      }
-
-      if ($parentclass == $class)
-      {
-        return $parent;
-      }
-      elseif (is_callable(array($parent, 'get_parent')))
-      {
-        return $parent->get_parent($class);
-      }
     }
   }
 
