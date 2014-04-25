2413c03047808c5118e9392ef66a04f4cbb41ef0
Timothy Totten <2010@huri.net>
2014-04-25 11:55:57 -0700

    Enhanced Data Object base class. Supports several new features.

--- lib/nano4/data/object.php
+++ lib/nano4/data/object.php
@@ -46,15 +46,68 @@ abstract class Object
 {
   use \Nano4\Meta\ClassInfo;
 
-  protected $data = array();   // The actual data we represent.
-  protected $parent;           // Will be set if we have a parent object.
+  protected $parent;             // Will be set if we have a parent object.
+  protected $data       = [];    // The actual data we represent.
+  protected $newconst   = False; // If true, we enable the new constructor.
+  protected $constprops = [];    // Constructor property items.
 
-  public function __construct ($mixed=Null, $opts=array())
+  /**
+   * The public constructor. The default version simply forwards to the
+   * internal method, see below.
+   */
+  public function __construct ($mixed=Null, $opts=Null)
   {
+    $this->__construct_data($mixed, $opts);
+  }
+
+  /**
+   * Internal constructor method.
+   *
+   * Supports two forms, the original form has the data as the first
+   * parameter, and a set of options as the second.
+   *
+   * The second form sends the options as the first parameter,
+   * with a named 'data' option to specify the data. In this case,
+   * the second parameter should be left off.
+   */
+  protected function __construct_data ($mixed=Null, $opts=Null)
+  {
+    if (is_null($opts))
+    {
+      if (is_array($mixed) && $this->newconst)
+      {
+        $opts  = $mixed;
+        $mixed = isset($opts['data']) ? $opts['data'] : Null;
+      }
+      else
+      {
+        $opts = [];
+      }
+    }
+
+    // Set the parent if it's defined.
     if (isset($opts['parent']))
     {
       $this->parent = $opts['parent'];
     }
+
+    // Find any properties that we need to initialize.
+    $props   = $this->constprops;
+    $props[] = '__classid'; // Explicitly add __classid.
+    foreach ($props as $popt => $pname)
+    {
+      // Positional entries have the same property name and option name.
+      if (is_numeric($popt))
+      {
+        $popt = $pname;
+      }
+
+      if (property_exists($this, $pname) && isset($opts[$popt]))
+      {
+        $this->$pname = $opts[$popt];
+      }
+    }
+
     if (method_exists($this, 'data_init'))
     { // The data_init can set up pre-requisites to loading our data.
       // It CANNOT reference our data, as that has not been loaded yet.
@@ -64,14 +117,14 @@ abstract class Object
     // How we proceed depends on if we have initial data.
     if (isset($mixed))
     { // Load the passed data.
-      $loadopts = array('clear'=>False, 'prep'=>True, 'post'=>True);
+      $loadopts = ['clear'=>False, 'prep'=>True, 'post'=>True];
       if (isset($opts['type']))
       {
         $loadopts['type'] = $opts['type'];
       }
       $this->load($mixed, $loadopts);
     }
-    elseif (is_callable(array($this, 'data_defaults')))
+    elseif (is_callable([$this, 'data_defaults']))
     { 
       if (!isset($opts['nodefaults']) || !$opts['nodefaults'])
       {
@@ -88,7 +141,7 @@ abstract class Object
   }
 
   // Returns the converted data structure.
-  public function load_data ($data, $opts=array())
+  public function load_data ($data, $opts=[])
   {
     $return = Null;
     // If we set the 'prep' option, send the data to data_prep()
@@ -138,7 +191,7 @@ abstract class Object
   }
 
   // Set our data to the desired structure.
-  public function load ($data, $opts=array())
+  public function load ($data, $opts=[])
   { // If we set the 'clear' option, clear our any existing data.
     if (isset($opts['clear']) && $opts['clear'])
     {
@@ -158,13 +211,13 @@ abstract class Object
   }
 
   // Clear our data.
-  public function clear ($opts=array())
+  public function clear ($opts=[])
   {
-    $this->data = array();
+    $this->data = [];
   }
 
   // Spawn a new empty data object.
-  public function spawn ($opts=array())
+  public function spawn ($opts=[])
   {
     $copy = clone $this;
     $copy->clear();
