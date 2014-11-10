12ff94cfd5633d5d985ba980d48103ad6f1f2a96
Timothy Totten <2010@huri.net>
2014-11-10 12:03:20 -0800

    Added Browser::get_ie_ver()

--- lib/nano4/utils/browser.php
+++ lib/nano4/utils/browser.php
@@ -16,5 +16,29 @@ class Browser
     }
     return False;
   }
+
+  /**
+   * Returns a number if it is a valid IE version.
+   *
+   * Returns False if the browser is not IE.
+   */
+  public static function get_ie_ver ()
+  {
+    preg_match('/MSIE (.*?);/', $_SERVER['HTTP_USER_AGENT'], $matches);
+    if (count($matches) < 2)
+    {
+      $trident = '/Trident\/\d{1,2}.\d{1,2}; rv:([0-9]*)/';
+      preg_match($trident, $_SERVER['HTTP_USER_AGENT'], $matches);
+    }
+
+    if (count($matches) > 1)
+    {
+      $version = $matches[1];
+      return $version;
+    }
+
+    return False;
+  }
+
 }
 
