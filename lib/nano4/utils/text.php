812ab096588786ead0f412aa3e2ef8641ad923a7
Timothy Totten <2010@huri.net>
2015-10-30 11:49:53 -0700

    Added max-length and threshold to make_identifier()

--- lib/nano4/utils/text.php
+++ lib/nano4/utils/text.php
@@ -4,9 +4,32 @@ namespace Nano4\Utils;
 
 class Text
 {
-  static function make_identifier ($string)
+  static function make_identifier ($string, $maxlen=null, $threshold=5)
   {
-    return
-      preg_replace('/[^A-Za-z_0-9]*/', '', preg_replace('/\s+/', '_', $string));
+    $ident = preg_replace('/[^A-Za-z_0-9]*/', '', preg_replace('/\s+/', '_', $string));
+
+    if (isset($maxlen) && is_numeric($maxlen) && $maxlen > 0)
+    {
+      $len = strlen($ident);
+
+      if ($len > $maxlen)
+      {
+        if ($len > ($maxlen + $threshold))
+        {
+          $size = (($maxlen/2)-1);
+          $str1 = substr($ident, 0, $size);
+          $size *= -1;
+          $str2 = substr($ident, $size);
+          $ident = $str1 . '__' . $str2;
+        }
+        else
+        {
+          $ident = substr($ident, 0, $maxlen);
+        }
+      }
+    }
+    
+    return $ident;
   }
 }
+
