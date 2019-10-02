a71ee0dacc1a37ef5f7074f63b7a68777f2760e9
Timothy Totten <2010@huri.net>
2019-10-02 10:50:46 -0700

    Removed Google currency converter, updated OpenExchangeRates.

--- lib/nano/utils/currency/openexchangerates.php
+++ lib/nano/utils/currency/openexchangerates.php
@@ -5,14 +5,53 @@ namespace Nano\Utils\Currency;
 /**
  * Get exchange rates and conversions using the OpenExchangeRates API.
  */
-
 class OpenExchangeRates
 {
   protected $base;
   protected $rates;
 
+  public static function getJson ($appId, $decoded=false)
+  {
+    if (!isset($appId))
+    {
+      throw new \Exception("Missing OpenExchangeRates AppId");
+    }
+    $curl = new \Nano\Utils\Curl();
+    $curl->strict = true;
+    $url  = "https://openexchangerates.org/api/latest.json?app_id=$appId";
+    $text = $curl->get($url);
+    if ($decoded)
+    {
+      $json = json_decode($text, true);
+      if (!is_array($json))
+      {
+        throw new \Exception("Invalid response from OpenExchangeRates");
+      }
+      return $json;
+    }
+    return $text;
+  }
+
+  public static function updateConfig ($outputFile, $appId)
+  {
+    if (!is_writable($outputFile))
+    {
+      throw new \Exception("Config file '$outputFile' is not writable");
+    }
+    $configText = self::getJson($appId);
+    file_put_contents($outputFile, $configText);
+  }
+
   public function __construct ($config)
   {
+    if (is_string($config))
+    { // Assume it's the appId, we'll get the rates from the API.
+      $config = self::getJson($config, true);
+    }
+    if (!is_array($config))
+    { // Something obviously went wrong somewhere.
+      throw new \Exception("OpenExchangeRates must be passed an AppId string or configuraton array");
+    }
     if (isset($config['base']) && isset($config['rates']))
     {
       $this->base  = $config['base'];
